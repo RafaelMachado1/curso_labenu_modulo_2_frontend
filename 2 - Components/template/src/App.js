3502d1dbfb8966ca40f4549c81ffa5cfcb412bec
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-06-13 17:02:01 -0300

    exercicio-template-concluido

---
+++ 2 - Components/template/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Garagem from "./componentes/Garagem";
+import Footer from "./componentes/Footer";
+import Header from "./componentes/Header";
+
+
+
+
+
+export default function App() {
+
+
+  return (
+    <div>
+      <Header />
+      <h1>Aula - Componentes React</h1>
+      <Garagem />
+      <Footer />
+
+    </div>
+  );
+}
