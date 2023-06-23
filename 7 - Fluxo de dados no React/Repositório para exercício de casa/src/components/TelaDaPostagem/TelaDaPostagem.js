4dbf11a793fe7d1fa6daef5e7aeaa05767cb02b3
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-06-23 13:54:59 -0300

    exercicio-concluido

---
+++ 7 - Fluxo de dados no React/Repositório para exercício de casa/src/components/TelaDaPostagem/TelaDaPostagem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { TitleHeader } from "../Header/styled";
+import { ContainerPostagem, Image, Description } from "./styles";
+
+const TelaDaPostagem = (props) => {
+  console.log("TelaDaPostagem(props)", props);
+
+  
+  return (
+    <ContainerPostagem>
+      <TitleHeader>{props.cadastro.titulo}</TitleHeader>
+      <Image src={props.cadastro.imagem} />
+      <Description>{props.cadastro.descricao}</Description>
+    </ContainerPostagem>
+  );
+};
+
+export default TelaDaPostagem;
